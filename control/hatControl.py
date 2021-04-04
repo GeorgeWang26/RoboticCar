26808c38e6ba0689a7b33377538ed6b6b0e6d773
WANG Hai <hai.wang2006@gmail.com>
2021-04-04 19:42:26 -0400

    baterry problem solved for servo hat

--- control/hatControl.py
+++ control/hatControl.py
@@ -1,13 +1,18 @@
 from adafruit_servokit import ServoKit
 # pip3 install adafruit-circuitpython-servokit
-
+import time
 # init 16 channels hat
 kit = ServoKit(channels=16)
 
-# x at channel 0
-# y at  channel 1
-x = kit.servo[0]
-y = kit.servo[1]
+# x at channel 1
+# y at  channel 0
+x = kit.servo[1]
+y = kit.servo[0]
+
+# left side of x >90
+# right side of x <90
+# up side of y <90
+# down side of y >90
 
 # 500 microseconds  = 0 deg
 # 1500 microseconds = 90 deg
@@ -15,9 +20,6 @@ y = kit.servo[1]
 x.set_pulse_width_range(500,2500)
 y.set_pulse_width_range(500,2500)
 
-# init at 90 deg(flat)
-x.angle = 90
-y.angle = 90
 
 def xChange(angle):
     x.angle = angle
@@ -26,5 +28,20 @@ def yChange(angle):
     y.angle = angle
 
 def close():
-    x.angle = 0
-    y.angle = 0
+    x.angle = 90
+    y.angle = 90
+
+# init at 90 deg(flat)
+x.angle = 90
+y.angle = 90
+
+if __name__ == '__main__':
+    time.sleep(3)
+    y.angle = 60
+    time.sleep(3)
+    y.angle = 90
+    time.sleep(3)
+    y.angle = 110
+    time.sleep(3)
+    x.angle = 90
+    y.angle = 90
